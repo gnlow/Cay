9a7a121fb12fac024e55252ba405a3af2809424e
Gnlow <gnlow@protonmail.com>
2024-04-29 14:39:05 +0900

    meh

--- serve.ts
+++ serve.ts
@@ -1,3 +1,22 @@
+const [ path ] = Deno.args
+
+/*
+
+import { walk } from "https://deno.land/std@0.224.0/fs/walk.ts"
+
+for await (const entry of walk(path)) {
+    if (entry.isFile) {
+        const path = "./" + entry.path.replaceAll("\\", "/")
+    }
+}
+
+*/
+
 const { default: v } = await import("./example/basic/index.tsx")
 
 console.log(v)
+
+Deno.serve(req => {
+    console.log(new URL(req.url).pathname)
+    return new Response("Hello, World!")
+})
