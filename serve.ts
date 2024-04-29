e28671610ee244562842642af33c6597b6f207c7
Gnlow <gnlow@protonmail.com>
2024-04-29 15:32:31 +0900

    Update serve.ts

--- serve.ts
+++ serve.ts
@@ -1,22 +1,12 @@
-const [ path ] = Deno.args
+const [ dir ] = Deno.args
 
-/*
-
-import { walk } from "https://deno.land/std@0.224.0/fs/walk.ts"
-
-for await (const entry of walk(path)) {
-    if (entry.isFile) {
-        const path = "./" + entry.path.replaceAll("\\", "/")
-    }
-}
-
-*/
-
-const { default: v } = await import("./example/basic/index.tsx")
-
-console.log(v)
-
-Deno.serve(req => {
-    console.log(new URL(req.url).pathname)
-    return new Response("Hello, World!")
+Deno.serve(async req => {
+    const path = "./"
+        + dir
+        + "/"
+        + (new URL(req.url).pathname.substring(1) || "index")
+        + ".tsx"
+    console.log(path)
+    const { default: page } = await import(path)
+    return new Response(page)
 })
